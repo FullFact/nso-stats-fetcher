8d54e1a5bf36c6f0d7eb4de6cb3b9392b2947fae
Fionntán O'Donnell <fionntan@fastmail.com>
2022-04-06 14:32:11 +0100

    reformatted PPI - cleaner code

--- nsofetch/fetch_za_stats.py
+++ nsofetch/fetch_za_stats.py
@@ -1,14 +1,12 @@
 import csv
 import itertools
 
-from numpy import NaN 
 import tabula
 import pandas
+import numpy
 
 import filepaths
 import utils
-
-MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 def fetch_za_inflation_cpi():
     stats_metadata = utils.read_stats_metadata()
@@ -25,15 +23,13 @@ def fetch_za_inflation_cpi():
     years = list(itertools.chain(*[[i]*12 for i in df.Year.tolist()]))
     months = [f"{i:02}" for i in range(1, 13)] * len(df.Year)
 
-    from locale import atof, setlocale, LC_NUMERIC
-    setlocale(LC_NUMERIC, '')
-
     observations = []
     for row in df.iterrows():
         observations += row[1]['Jan':'Dec'].tolist()
 
     output_df = pandas.DataFrame({'year': years, 'month': months, 'observations': observations})
     output_df.dropna(inplace=True)
+    # convert from ZA format of commas for decimal places to dots
     output_df.observations = output_df.observations.str.replace(',', '.')
 
     output_filepath = filepaths.DATA_DIR / stats_metadata['ZA']['inflation']['CPI']['filename']
@@ -43,34 +39,32 @@ def fetch_za_inflation_cpi():
 def fetch_za_inflation_ppi():
     stats_metadata = utils.read_stats_metadata()
 
-    url = stats_metadata['ZA']['inflation']['CPI']['url']
+    url = stats_metadata['ZA']['inflation']['PPI']['url']
     tables = tabula.read_pdf(url, pages="all", multiple_tables=False)
+    df = tables[0]
 
-    # tables are split between two PDF pages
-    za_ppi_df = tables[0]
-    # drop Average column 
-    za_ppi_df.drop('Average', axis=1, inplace=True)
-    # this PDF table is so awkward I can't see a way to get data without hardcoding
-    years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-    table_row_inds = [4, 6, 8, 10, 12, 14, 16, 18, 20, 24]
-    table_months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    csv_data = [['year', 'month', 'observation']]
-    for row_ind, year in zip(table_row_inds, years):
-        for month_ind, month in enumerate(table_months):
-            # table cell value
-            val = za_ppi_df.at[row_ind, table_months[month_ind]]
-            # ignore blank values
-            if val == '..': continue
-            val = float(val.replace(',', '.'))
-            csv_data.append([year, MONTHS[month_ind], val])
+    # very awkward year column to read
+    years = [a.replace(' Index', '') for a in df['Year and'].tolist() if a not in ['type', 'Rate', 'Index', numpy.nan]]
+    months = [f"{i:02}" for i in range(1, 13)] * len(years)
+    years = list(itertools.chain(*[[i]*12 for i in years]))
+
+    observations = []
+    for row_tuple in df.itertuples():
+        if row_tuple._1 != 'Rate':
+            continue
+        observations += [row_tuple.Jan, row_tuple.Feb, row_tuple.Mar, row_tuple.Apr, row_tuple.May, row_tuple.Jun, 
+            row_tuple.Jul, row_tuple.Aug, row_tuple.Sep, row_tuple.Oct, row_tuple.Nov, row_tuple.Dec]
+
+    output_df = pandas.DataFrame({'year': years, 'month': months, 'observations': observations})
+    # this PDF table marks missing values with '..' - so drop those
+    output_df = output_df[output_df.observations != '..']
+    # convert from ZA format of commas for decimal places to dots
+    output_df.observations = output_df.observations.str.replace(',', '.')
 
     output_filepath = filepaths.DATA_DIR / stats_metadata['ZA']['inflation']['PPI']['filename']
-    with open(output_filepath, 'w') as csvfile: 
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(csv_data)
-
+    output_df.to_csv(output_filepath, index=False)
 
 
 if __name__ == '__main__':
-    fetch_za_inflation_cpi()
-    # fetch_za_inflation_ppi()
+    # fetch_za_inflation_cpi()
+    fetch_za_inflation_ppi()
