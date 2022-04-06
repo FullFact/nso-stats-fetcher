733f2631cf75e47c11bac3efe83449fe2823f639
Fionntán O'Donnell <fionntan@fastmail.com>
2022-04-06 10:32:49 +0100

    refactored repeated methods out

--- nsofetch/fetch_uk_stats.py
+++ nsofetch/fetch_uk_stats.py
@@ -5,15 +5,12 @@ import pandas
 import filepaths
 import utils
 
-def fetch_uk_cpih():
-    stats_metadata = utils.read_stats_metadata()
-
-    url = stats_metadata['UK']['inflation']['CPIH']['url']
+def fetch_uk_stat(url: str, skiprows: int, output_filepath: str):
     tmp_filepath = utils.download_csv(url)
 
-    # skip first 173 rows to get month data, and special formatting for dates
+    # skip first X rows to get month data, and special formatting for dates
     custom_date_parser = lambda x: datetime.strptime(x, "%Y %b")
-    df = pandas.read_csv(tmp_filepath, skiprows=173, header=None, names=['date', 'observation'], 
+    df = pandas.read_csv(tmp_filepath, skiprows=skiprows, header=None, names=['date', 'observation'], 
         parse_dates=['date'], date_parser=custom_date_parser)
 
     output_df = pandas.DataFrame(
@@ -21,46 +18,32 @@ def fetch_uk_cpih():
         'month': df['date'].dt.strftime('%b'), 
         'observation': df['observation']}
     ).copy()
-    output_filepath = filepaths.DATA_DIR / stats_metadata['UK']['inflation']['CPIH']['filename']
     output_df.to_csv(output_filepath, index=False)
 
+def fetch_uk_cpih():
+    stats_metadata = utils.read_stats_metadata()
+    skiprows = 173
+    url = stats_metadata['UK']['inflation']['CPIH']['url']
+    output_filepath = filepaths.DATA_DIR / stats_metadata['UK']['inflation']['CPIH']['filename']
+
+    fetch_uk_stat(url, skiprows, output_filepath)
 
 def fetch_uk_cpi():
     stats_metadata = utils.read_stats_metadata()
-
-    url = stats_metadata['UK']['inflation']['CPI']['url']
-    tmp_filepath = utils.download_csv(url)
-
-    custom_date_parser = lambda x: datetime.strptime(x, "%Y %b")
-    df = pandas.read_csv(tmp_filepath, skiprows=173, header=None, names=['date', 'observation'], 
-        parse_dates=['date'], date_parser=custom_date_parser)
-
-    output_df = pandas.DataFrame(
-        {'year': df['date'].dt.year, 
-        'month': df['date'].dt.strftime('%b'), 
-        'observation': df['observation']}
-    ).copy()
+    url = stats_metadata['UK']['inflation']['CPIH']['url']
+    skiprows = 173
     output_filepath = filepaths.DATA_DIR / stats_metadata['UK']['inflation']['CPI']['filename']
-    output_df.to_csv(output_filepath, index=False)
+    
+    fetch_uk_stat(url, skiprows, output_filepath)
 
 
 def fetch_uk_rpi():
     stats_metadata = utils.read_stats_metadata()
-
     url = stats_metadata['UK']['inflation']['RPI']['url']
-    tmp_filepath = utils.download_csv(url)
-
-    custom_date_parser = lambda x: datetime.strptime(x, "%Y %b")
-    df = pandas.read_csv(tmp_filepath, skiprows=375, header=None, names=['date', 'observation'], 
-        parse_dates=['date'], date_parser=custom_date_parser)
-
-    output_df = pandas.DataFrame(
-        {'year': df['date'].dt.year, 
-        'month': df['date'].dt.strftime('%b'), 
-        'observation': df['observation']}
-    ).copy()
+    skiprows = 375
     output_filepath = filepaths.DATA_DIR / stats_metadata['UK']['inflation']['RPI']['filename']
-    output_df.to_csv(output_filepath, index=False)
+    
+    fetch_uk_stat(url, skiprows, output_filepath)
 
 if __name__ == '__main__':
     fetch_uk_cpih()
