0e82081f518f9c1b283b1d0ccb92c556b2d3874c
Fionntán O'Donnell <fionntan@fastmail.com>
2022-04-25 11:53:25 +0100

    south africa inflation data in the json file now

--- nsofetch/filepaths.py
+++ nsofetch/filepaths.py
@@ -5,9 +5,3 @@ ROOT_DIR = Path(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 DATA_DIR = ROOT_DIR / 'data'
 
 NSO_STATS_METADATA = ROOT_DIR / 'data' / 'nso_stats_metadata.json'
-
-# Statistics South Africa
-ZA_INFLATION_CPI_DATA = ROOT_DIR / 'data' / 'za_inflation_cpi.csv'
-ZA_INFLATION_CPI_INFO = ROOT_DIR / 'data' / 'za_inflation_cpi.json'
-ZA_INFLATION_PPI_DATA = ROOT_DIR / 'data' / 'za_inflation_ppi.csv'
-ZA_INFLATION_PPI_INFO = ROOT_DIR / 'data' / 'za_inflation_ppi.json'
