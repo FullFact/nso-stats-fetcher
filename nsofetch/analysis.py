6862f3664c933955ae9921d0e37892322ec0fffa
Fionntán O'Donnell <fionntan@fastmail.com>
2022-04-27 11:39:55 +0100

    added plots for ZA and NG

--- nsofetch/analysis.py
+++ nsofetch/analysis.py
@@ -11,22 +11,33 @@ import filepaths
 def plot_inflation_stats():
     stats_metadata = utils.read_stats_metadata()
 
-    input_filepath = filepaths.DATA_DIR / stats_metadata['UK']['inflation']['CPI']['filename']
-    df_uk_inflation = pandas.read_csv(input_filepath, index_col=0)
-
     input_filepath = filepaths.DATA_DIR / stats_metadata['AR']['inflation']['CPI']['filename']
-    df_ar_inflation = pandas.read_csv(input_filepath, index_col=0)
+    df_ar_inflation = pandas.read_csv(input_filepath, index_col=0, parse_dates=['month'], infer_datetime_format=True)
 
-    start = df_uk_inflation.index[0]
-    end = df_uk_inflation.index[-1]
+    input_filepath = filepaths.DATA_DIR / stats_metadata['UK']['inflation']['CPI']['filename']
+    df_uk_inflation = pandas.read_csv(input_filepath, index_col=0, parse_dates=['month'], infer_datetime_format=True)
+
+    input_filepath = filepaths.DATA_DIR / stats_metadata['NG']['inflation']['CPI']['filename']
+    df_ng_inflation = pandas.read_csv(input_filepath, index_col=0, parse_dates=['month'], infer_datetime_format=True)
+
+    input_filepath = filepaths.DATA_DIR / stats_metadata['ZA']['inflation']['CPI']['filename']
+    df_za_inflation = pandas.read_csv(input_filepath, index_col=0, parse_dates=['month'], infer_datetime_format=True)
 
     # fix this so that it's a combination of all the indexes
-    idx = df_uk_inflation.index
+    idx = df_za_inflation.index
 
-    df_plot = pandas.DataFrame({'Argentina': df_ar_inflation.observation, 'UK': df_uk_inflation.observation}, index=idx)
+    df_plot = pandas.DataFrame(
+        {
+            'Argentina': df_ar_inflation.observation, 
+            'Nigeria': df_ng_inflation.observation, 
+            'UK': df_uk_inflation.observation, 
+            'South Africa': df_za_inflation.observation
+        }, 
+        index=idx
+    )
 
     output_filepath = filepaths.DATA_DIR / 'inflation_stats.html'
-    plot = df_plot.hvplot(y=['Argentina', 'UK'], value_label='Yearly inflation % (CPI)', legend='top', height=500, width=620)
+    plot = df_plot.hvplot(y=['Argentina', 'Nigeria', 'UK', 'South Africa'], value_label='Yearly inflation % (CPI)', legend='top', height=500, width=620)
     hvplot.save(plot, output_filepath)
 
 
