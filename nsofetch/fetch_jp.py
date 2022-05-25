145eb96d1d84b2f83a2e34986e7cd02fe2bea627
Fionntán O'Donnell <fionntan@fastmail.com>
2022-05-25 17:28:13 +0100

    starting japan

---
+++ nsofetch/fetch_jp.py
@@ -0,0 +1,15 @@
+import pandas 
+
+import utils
+
+def fetch_jp_inflation_cpi():
+    stats_metadata = utils.read_stats_metadata()
+    url = stats_metadata['IE']['inflation']['CPI']['url']
+
+    tmp_filepath = utils.download_file(url)
+
+    df = pandas.read_excel(tmp_filepath, sheet_name='am01-1 (3)', header=None)
+
+
+if __name__ == '__main__':
+    fetch_jp_inflation_cpi()
