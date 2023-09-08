31d182d2c12d7649ee80c5fd6a2d328a98f1ac6b
David Corney <dcorney@gmail.com>
2023-09-08 13:54:12 +0100

    Reverting last change; didn't fix problem

--- src/nsofetch/fetch_ar.py
+++ src/nsofetch/fetch_ar.py
@@ -1,5 +1,5 @@
 import pandas
-import requests 
+import requests
 
 import utils
 import filepaths
@@ -8,23 +8,25 @@ import filepaths
 def fetch_ar_inflation_cpi():
     stats_metadata = utils.read_stats_metadata()
 
-    url = stats_metadata['AR']['inflation']['CPI']['url']
+    url = stats_metadata["AR"]["inflation"]["CPI"]["url"]
     response = requests.get(url)
     response.raise_for_status()
     stats = response.json()
 
     months = []
     observations = []
-    for row in stats['data']:
+    for row in stats["data"]:
         # drop the day number from data: eg. '2020-01-01' becomes '2020-01'
         months.append(row[0][:7])
         # convert decimal to percentage
         observations.append(row[1] * 100)
 
-    output_df = pandas.DataFrame({'month': months, 'observation': observations})
-    output_filepath = filepaths.DATA_DIR / stats_metadata['AR']['inflation']['CPI']['filename']
+    output_df = pandas.DataFrame({"month": months, "observation": observations})
+    output_filepath = (
+        filepaths.DATA_DIR / stats_metadata["AR"]["inflation"]["CPI"]["filename"]
+    )
     output_df.to_csv(output_filepath, index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fetch_ar_inflation_cpi()
